54ef30bc29b59568d3edc22cbf313df16774518d
igoodwill <igor.goodwill1@gmail.com>
2019-03-03 18:52:52 +0200

    [practice-4] Pizza list

--- Frontend/www/assets/js/main.js
+++ Frontend/www/assets/js/main.js
@@ -237,8 +237,8 @@ exports.PizzaMenu_OneItem = ejs.compile(`
   %>
   <div class="col-sm-6 col-lg-4 pizza-card">
     <div class="thumbnail">
+      <img class="pizza-icon" src="<%= pizza.icon %>" alt="Pizza">
       <div class="pizza-info">
-        <img class="pizza-icon" src="<%= pizza.icon %>" alt="Pizza">
         <% if(pizza.is_new) { %>
           <span class="label label-danger">
             Нова
@@ -456,6 +456,62 @@ var Templates = require('../Templates');
 var PizzaCart = require('./PizzaCart');
 var Pizza_List = require('../Pizza_List');
 
+var $currentType = $("#type");
+var $count = $("#count");
+var $types = $("#types");
+
+const types = [
+  {
+    type: "Мясні",
+    filter: {
+      require: ["meat", "chicken"]
+    }
+  }, {
+    type: "З ананасами",
+    filter: {
+      require: ["pineapple"]
+    }
+  }, {
+    type: "З грибами",
+    filter: {
+      require: ["mushroom"]
+    }
+  }, {
+    type: "З морепродуктами",
+    filter: {
+      require: ["ocean"]
+    }
+  }, {
+    type: "Вега",
+    filter: {
+      forbid: ["meat", "chicken", "ocean"]
+    }
+  }
+];
+
+function findActiveType() {
+  return $types.find(".active");
+}
+
+function changeType(typeHtml, type) {
+  if ($currentType.text() != type) {
+    var currentType = findActiveType();
+    currentType.removeClass("active");
+    typeHtml.addClass("active");
+    $currentType.text(type);
+  }
+}
+
+function addType({type, filter}) {
+  var typeHtml = $(`<li>${type}</li>`);
+  typeHtml.click(function() {
+    changeType(typeHtml, type);
+    filterPizza(filter);
+  });
+
+  $types.append(typeHtml);
+}
+
 //HTML едемент куди будуть додаватися піци
 var $pizza_list = $("#pizza_list");
 
@@ -481,18 +537,34 @@ function showPizzaList(list) {
     }
 
     list.forEach(showOnePizza);
+    $count.text(list.length);
+}
+
+function filterByContent(content, {forbid, require}) {
+  if (forbid) {
+    for (var i = 0; i < forbid.length; i++) {
+      if (forbid[i] in content) {
+        return false;
+      }
+    }
+  }
+
+  if (require) {
+    for (var i = 0; i < require.length; i++) {
+      if (require[i] in content) {
+        return true;
+      }
+    }
+  } else {
+    return true;
+  }
+
+  return false;
 }
 
 function filterPizza(filter) {
     //Масив куди потраплять піци які треба показати
-    var pizza_shown = [];
-
-    Pizza_List.forEach(function(pizza){
-        //Якщо піка відповідає фільтру
-        //pizza_shown.push(pizza);
-
-        //TODO: зробити фільтри
-    });
+     var pizza_shown = Pizza_List.filter(pizza => filterByContent(pizza.content, filter));
 
     //Показати відфільтровані піци
     showPizzaList(pizza_shown);
@@ -500,7 +572,14 @@ function filterPizza(filter) {
 
 function initialiseMenu() {
     //Показуємо усі піци
-    showPizzaList(Pizza_List)
+    showPizzaList(Pizza_List);
+
+    types.forEach(addType);
+    var $allType = findActiveType();
+    $allType.click(function() {
+      changeType($allType, $allType.text());
+      showPizzaList(Pizza_List);
+    });
 }
 
 exports.filterPizza = filterPizza;
